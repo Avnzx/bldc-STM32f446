7fd35d6fe0b02862f29ff570a00bf48d54043bfe
Aavin Fernando <aavin.fernando@gmail.com>
2025-11-26 18:53:03 +1100

    Disable GPIOI bank, Disable I2C Init, Change Xtal

--- ChibiOS_3.0.5/os/hal/ports/STM32/STM32F4xx/stm32_registry.h
+++ ChibiOS_3.0.5/os/hal/ports/STM32/STM32F4xx/stm32_registry.h
@@ -414,7 +414,7 @@
 #define STM32_HAS_GPIOH                     TRUE
 #define STM32_HAS_GPIOF                     TRUE
 #define STM32_HAS_GPIOG                     TRUE
-#define STM32_HAS_GPIOI                     TRUE
+#define STM32_HAS_GPIOI                     FALSE 
 #define STM32_GPIO_EN_MASK                  (RCC_AHB1ENR_GPIOAEN |          \
                                              RCC_AHB1ENR_GPIOBEN |          \
                                              RCC_AHB1ENR_GPIOCEN |          \
